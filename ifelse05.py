527fd8fc8b09e0664f64e623fb2429dbccd6d5bf
muhammadali727 <suyunovmuhammadali89@gmail.com>
2025-06-20 14:22:48 +0300

    done

--- ifelse05.py
+++ ifelse05.py
@@ -1,1 +1,7 @@
+ememail = input("Email manzilingizni kiriting: ")
 
+if "@" in email and (email.endswith(".com") or email.endswith(".uz") or email.endswith(".net") or email.endswith(".org")):
+    print("Email qabul qilindi.")
+
+if "@" not in email or not (email.endswith(".com") or email.endswith(".uz") or email.endswith(".net") or email.endswith(".org")):
+    print("Email noto'g'ri formatda.")
