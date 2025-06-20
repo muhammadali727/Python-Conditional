bc756fccc55ae3502c89f8f58ea7a1ec55b0b1d4
muhammadali727 <suyunovmuhammadali89@gmail.com>
2025-06-20 14:10:46 +0300

    done

--- elif04.py
+++ elif04.py
@@ -1,1 +1,14 @@
 
+yosh = int(input("Yoshingizni kiriting: "))
+narx = 100
+
+if yosh <= 6:
+    narx = narx * 0.5
+
+if yosh >= 7 and yosh <= 17:
+    narx = narx * 0.8
+
+if yosh > 60:
+    narx = narx * 0.7
+
+print("Yakuniy narx:", narx, "so'm")
