b7e5062e054669b6a0137cc8c48edb56495a6650
muhammadali727 <suyunovmuhammadali89@gmail.com>
2025-06-20 14:14:11 +0300

    done

--- elif05.py
+++ elif05.py
@@ -1,1 +1,10 @@
+harorat = float(input("Haroratni kiriting (°C): "))
 
+if harorat < 0:
+    print("Juda sovuq! Issiq kiyim kiying.")
+
+if harorat >= 0 and harorat <= 14:
+    print("Sovuq. Kurtka kiying.")
+
+if harorat >= 15:
+    print("Ob-havo yaxshi.")
