0e60a6d0108cad606ba0c65d257424a6444788c7
muhammadali727 <suyunovmuhammadali89@gmail.com>
2025-06-20 14:08:48 +0300

    done

--- elif03.py
+++ elif03.py
@@ -1,1 +1,10 @@
+son = int(input("Son kiriting: "))
 
+if son % 2 == 0:
+    print(son, "soni 2 ga bo'linadi")
+
+if son % 3 == 0:
+    print(son, "soni 3 ga bo'linadi")
+
+if son % 5 == 0:
+    print(son, "soni 5 ga bo'linadi")
