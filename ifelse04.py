0b1bf3f8134ae19eec84eb9001cd22782185a990
muhammadali727 <suyunovmuhammadali89@gmail.com>
2025-06-20 14:19:25 +0300

    done

--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,9 @@
+balans = 5000
+summa = int(input("Yechmoqchi bo'lgan summani kiriting: "))
 
+if summa <= balans:
+    balans = balans - summa
+    print(f"Pul yechildi. Qolgan balans: {balans} so'm")
+
+if summa > balans:
+    print(f"Mablag' yetarli emas. Sizning balansingiz: {balans} so'm")
